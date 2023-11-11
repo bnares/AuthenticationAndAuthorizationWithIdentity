187ba7106c1e0922433ab2af7a1ab046171690f4
bnares <piotr.ostrouch.programista@gmail.com>
2023-11-11 19:30:25 +0100

    first

---
+++ ArtGuard/Models/Domain/ArtGuardDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Primitives;
+
+namespace ArtGuard.Models.Domain
+{
+    public class ArtGuardDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ArtGuardDbContext(DbContextOptions<ArtGuardDbContext> options) : base(options) 
+        {
+
+        }
+    }
+}
